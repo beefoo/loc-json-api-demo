97b713bbf15d9f5ddd08bb84c3267c4ae33d4a3e
beefoo <beefoo@users.noreply.github.com>
2024-03-06 17:38:00 +0000

    deploy: c1edd1203b074bddd0fd342b01eb939ee86a293a

--- _static/loc.js
+++ _static/loc.js
@@ -70,6 +70,71 @@ function locMain() {
             lmain.setAttribute('aria-label', 'Primary');
         });
     }
+
+    // inject alt text for generated images if captions are available
+    // HACK: captions are <p> elements with <em> text inside of it. They should immediately follow the output cell that contains an image 
+    const images = document.querySelectorAll('#article-main .cell_output img');
+    if (images) {
+        images.forEach((image) => {
+            const cell = image.closest('.cell');
+            // check to see if the next sibling is a paragraph tag
+            const {nextElementSibling} = cell;
+            const {nodeName} = nextElementSibling;
+            if (nodeName === "P") {
+                // check to see if there is an emphasis inside of it
+                const em = nextElementSibling.getElementsByTagName("em");
+                if (em && em.length > 0) {
+                    const {innerText} = em[0];
+                    // set the alt text of the image with the emphasized text
+                    image.setAttribute('alt', innerText);
+                    // remove the source element
+                    nextElementSibling.remove();
+                }
+            }
+        });
+    }
+
+    // Make sidebar toggles focus-able
+    const sidebarToggles = document.querySelectorAll('label.sidebar-toggle');
+    if (sidebarToggles) {
+        sidebarToggles.forEach((toggle) => {
+            toggle.setAttribute('tabindex', 0);
+            // trigger click on enter
+            toggle.addEventListener('keyup', event => {
+                if(event.key !== 'Enter') return;
+                toggle.click();
+                toggle.focus();
+                event.preventDefault();
+            });
+        });
+    }
+
+    // Make tables accessible
+    const dataTables = document.querySelectorAll('table.dataframe');
+    if (dataTables) {
+        dataTables.forEach((table) => {
+            const ths = table.querySelectorAll('thead th');
+            ths.forEach((th) => {
+              const text = th.innerText.trim();
+               // Put content in empty table heading
+              if (text.length === 0) {
+                th.innerText = '#';
+              }
+              // Set heading scope
+              th.setAttribute('scope', 'col');
+            });
+
+            // Attempt to add title to table based on the first element in the section (should be a heading)
+            const section = table.closest('section');
+            const sectionId = section.getAttribute('id');
+            if (sectionId && sectionId !== '') {
+                const headingId = `${sectionId}-heading`;
+                const sectionHeading = section.querySelector('h1, h2, h3, h4, h5, h6');
+                sectionHeading.setAttribute('id', headingId);
+                table.setAttribute('aria-describedby', headingId);
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
